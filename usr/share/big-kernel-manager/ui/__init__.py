5cc18a64ab09bc615911a3709b9a42efa5528aa2
Bruno Gonçalves <bigbruno@gmail.com>
2026-03-06 14:59:58 -0300

    Moved

--- usr/share/big-kernel-manager/ui/__init__.py
+++
@@ -1,8 +0,0 @@
-"""UI package for Big Kernel Manager application."""
-
-from ui.base_page import BaseSection, BasePage
-
-__all__ = [
-    "BaseSection",
-    "BasePage",
-]
--- usr/share/big-kernel-manager/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Utils package for Big Kernel Manager application."""
-
-from utils.i18n import _
-
-__all__ = [
-    "_",
-]
